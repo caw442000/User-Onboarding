a820760ee0ba78836b6152519553e0d6edf3ee27
cedric-winbush <cawinbushjr@gmail.com>
2020-01-30 14:16:43 -0600

    add validation to name email password

--- src/components/Form.js
+++ src/components/Form.js
@@ -4,7 +4,14 @@ import * as Yup from "yup";
 import axios from "axios";
 
 
-const UserForm = ({values}) => {
+const UserForm = ({values, errors, touched, status}) => {
+  const [users, setUsers] = useState([]);
+
+  useEffect(() => {
+    console.log("status has changed!", status);
+
+    status && setUsers(users => [...users, status]);
+  }, [status]);
   return (
     <div>
       <Form>
@@ -12,21 +19,40 @@ const UserForm = ({values}) => {
         <Field id="name" type="text" name="name" />
         <label htmlFor= "email">Email:</label>
         <Field id="email" type="email" name="email" />
+        {touched.email && errors.email && (
+          <p className="errors">{errors.email}</p>
+        )}
         <label htmlFor= "password">Password:</label>
         <Field id="password" type="password" name="password" />
+        {touched.password && errors.password && (
+          <p className="errors">{errors.password}</p>
+        )}
         <label >
           Terms of Service
           <Field
             type="checkbox"
-            name="tos"
+            name="termsOfService"
             checked={values.termsOfService}
           />
+          {touched.termsOfService && errors.termsOfService && (
+          <p className="errors">{errors.termsOfService}</p>
+        )}
           <span className="checkmark" />
         </label>
         <button>Submit</button>
 
       </Form>
+      {users.map (user =>(
+        <ul key={user.id}>
+          <li>Name: {user.name}</li>
+          <li>Email: {user.email}</li>
+          <li>Password: {user.password}</li>
+        </ul>
 
+
+
+
+      ))}
     </div>
   );
 };
@@ -37,9 +63,15 @@ const FormikUserForm = withFormik({
     name:"",
     email: "",
     password:"",
-    termsOfService
+    termsOfService: termsOfService|| true
   };
 
   },
+  validationSchema: Yup.object().shape({
+    name: Yup.string().required(),
+    email: Yup.string().email().required(),
+    password: Yup.string().required()
+
+  }),
 })(UserForm);
 export default FormikUserForm;
